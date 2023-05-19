1d9d363d809f3aca0fe3bab8748318f0aa955228
Jordon Archer <jordon_services@hotmail.co.uk>
2023-05-19 14:58:28 +0100

    removed uneeded code

--- static/qrcode_generator.js
+++ static/qrcode_generator.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         text: This_page_url,
-        width: 150,
-        height: 150,
+        width: 200,
+        height: 200,
         colorDark : "#000000",
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
