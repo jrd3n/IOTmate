4599662fbbdc4cc5748423d0b377178a554adca0
Jordon Archer <jordon_services@hotmail.co.uk>
2023-05-19 14:45:48 +0100

    hides the qr code when viewed on a mobile

--- static/qrcode_generator.js
+++ static/qrcode_generator.js
@@ -8,3 +8,18 @@ document.addEventListener("DOMContentLoaded", function(){
         correctLevel : QRCode.CorrectLevel.H
     });
 });
+
+// Check if the user is on a mobile device
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+// Get the element you want to hide
+var nonMobileElement = document.getElementById('qrcode');
+
+// Hide the element if the user is on a mobile device
+if (isMobileDevice()) {
+  nonMobileElement.style.display = 'none';
+} else {
+  nonMobileElement.style.display = 'block';
+}
